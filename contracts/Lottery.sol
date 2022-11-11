59f4082e11e27504783a1c876498a595b047c8ab
grc02 <georgi.chonkov27@gmail.com>
2022-11-11 13:45:25 +0200

    Added a basic structure of the main functionality of the smart contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,4 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Lottery {}
+error Lottery__NotEnoughETH();
+
+contract Lottery {
+    uint256 private immutable i_entranceFee;
+    address[] internal s_participants;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterLottery() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughETH();
+        }
+        s_participants.push(msg.sender);
+    }
+
+    function pickWinner() external {}
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) external view returns (address) {
+        return s_participants[index];
+    }
+}

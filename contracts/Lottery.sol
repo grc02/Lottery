edf85f54a642f3ca5d220fa875c737f616e64322
grc02 <georgi.chonkov27@gmail.com>
2022-11-16 19:04:44 +0200

    CheckUpKeep functionality added in order to make the lottery fully automatable once deployed

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.17;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
 error Lottery__NotEnoughETH();
 error Lottery__TransferFailed();
 
-contract Lottery is VRFConsumerBaseV2 {
+contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
     uint256 private immutable i_entranceFee;
     address[] internal s_participants;
     VRFCoordinatorV2Interface private immutable i_VRFCoordinator;
@@ -16,6 +17,8 @@ contract Lottery is VRFConsumerBaseV2 {
     uint32 private immutable i_callbackGasLimit;
     uint32 private constant NUM_WORDS = 1;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint256 public lastTimeStamp;
+    uint256 public interval;
 
     address private s_latestWinner;
 
@@ -28,13 +31,16 @@ contract Lottery is VRFConsumerBaseV2 {
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 _interval
     ) VRFConsumerBaseV2(VRFCordinatorV2) {
         i_entranceFee = entranceFee;
         i_VRFCoordinator = VRFCoordinatorV2Interface(VRFCordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        interval = _interval;
+        lastTimeStamp = block.timestamp;
     }
 
     function enterLottery() external payable {
@@ -43,6 +49,20 @@ contract Lottery is VRFConsumerBaseV2 {
         }
         s_participants.push(msg.sender);
         emit lotteryEntered(msg.sender);
+    }
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
     }
 
     function requestRandomWinner() external {

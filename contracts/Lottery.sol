5da5e7d996ea5cf5f26253a8f7a5e7de9b545c83
grc02 <georgi.chonkov27@gmail.com>
2022-11-16 19:14:06 +0200

    Changed the names of some of the variables and created an enum struct to keep track of the states of the Lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,8 +7,14 @@ import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
 error Lottery__NotEnoughETH();
 error Lottery__TransferFailed();
+error Lottery__NotOpen();
 
 contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    enum LotteryState {
+        OPEN,
+        CALCULATING
+    }
+
     uint256 private immutable i_entranceFee;
     address[] internal s_participants;
     VRFCoordinatorV2Interface private immutable i_VRFCoordinator;
@@ -17,10 +23,10 @@ contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
     uint32 private immutable i_callbackGasLimit;
     uint32 private constant NUM_WORDS = 1;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
-    uint256 public lastTimeStamp;
-    uint256 public interval;
-
+    uint256 public s_lastTimeStamp;
+    uint256 public s_interval;
     address private s_latestWinner;
+    LotteryState private s_lotteryState;
 
     event lotteryEntered(address indexed participant);
     event randomnessRequested(uint256 indexed requestedId);
@@ -32,20 +38,25 @@ contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
         bytes32 gasLane,
         uint64 subscriptionId,
         uint32 callbackGasLimit,
-        uint256 _interval
+        uint256 interval
     ) VRFConsumerBaseV2(VRFCordinatorV2) {
         i_entranceFee = entranceFee;
         i_VRFCoordinator = VRFCoordinatorV2Interface(VRFCordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
-        interval = _interval;
-        lastTimeStamp = block.timestamp;
+        s_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        s_lotteryState = LotteryState.OPEN;
     }
 
     function enterLottery() external payable {
         if (msg.value < i_entranceFee) {
             revert Lottery__NotEnoughETH();
+        }
+
+        if (s_lotteryState != LotteryState(0)) {
+            revert Lottery__NotOpen();
         }
         s_participants.push(msg.sender);
         emit lotteryEntered(msg.sender);
@@ -62,10 +73,11 @@ contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
             bytes memory /* performData */
         )
     {
-        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+        upkeepNeeded = (block.timestamp - s_lastTimeStamp) > s_interval;
     }
 
     function requestRandomWinner() external {
+        s_lotteryState = LotteryState.CALCULATING;
         uint256 requestId = i_VRFCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
@@ -84,6 +96,8 @@ contract Lottery is VRFConsumerBaseV2, AutomationCompatibleInterface {
         uint256 winnerIndex = randomWords[0] % s_participants.length;
         address winner = s_participants[winnerIndex];
         s_latestWinner = winner;
+        s_lotteryState = LotteryState.OPEN;
+        s_participants = new address[](0);
 
         (bool success, ) = payable(winner).call{value: address(this).balance}(
             ""

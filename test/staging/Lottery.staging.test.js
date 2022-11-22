5691986ddbe84469b0887c782af3762a1764cb43
grc02 <georgi.chonkov27@gmail.com>
2022-11-22 11:49:03 +0200

    Completed the staging test

--- test/staging/Lottery.staging.test.js
+++ test/staging/Lottery.staging.test.js
@@ -1,26 +1,31 @@
 const { expect, assert } = require("chai");
-const { ethers, deployments, network } = require("hardhat");
-const { networkConfig, developmentChains } = require("../../helper-hardhat-config");
+const { ethers, network } = require("hardhat");
+const { developmentChains } = require("../../helper-hardhat-config");
 
 developmentChains.includes(network.name)
     ? describe.skip
     : describe("Lottery staging tests", function () {
           let lottery, entranceFee, deployer;
 
+          console.log("Entered staging tests--------------------");
+
           beforeEach(async () => {
               deployer = (await getNamedAccounts()).deployer;
               lottery = await ethers.getContract("Lottery", deployer);
 
               entranceFee = await lottery.getEntranceFee();
+              console.log(`Lottery address: ${lottery.address}`);
+
+              console.log("All initial vars are set--------------------");
           });
 
           describe("fulfillRandomWords", function () {
               it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async function () {
-                  console.log("Setting up test...");
-                  const startingTimeStamp = await lottery.getLastTimeStamp();
+                  console.log("Setting up test--------------------");
+                  const startingTimeStamp = await lottery.s_lastTimeStamp();
                   const accounts = await ethers.getSigners();
 
-                  console.log("Setting up Listener...");
+                  console.log("Setting up Listener--------------------");
                   await new Promise(async (resolve, reject) => {
                       lottery.once("WinnerPicked", async () => {
                           console.log("WinnerPicked event fired!");
@@ -31,25 +36,24 @@ developmentChains.includes(network.name)
                               const endingTimeStamp = await lottery.s_lastTimeStamp();
 
                               await expect(lottery.getPlayer(0)).to.be.reverted;
-                              assert.equal(latestWinner.toString(), accounts[1].address);
+                              assert.equal(latestWinner.toString(), accounts[0].address);
                               assert.equal(lotteryState, 0);
                               assert.equal(
                                   winnerEndingBalance.toString(),
                                   winnerStartingBalance.add(entranceFee).toString()
                               );
                               assert(endingTimeStamp > startingTimeStamp);
+                              console.log("Done, all asserts passed--------------------");
                               resolve();
                           } catch (error) {
                               console.log(error);
                               reject(error);
                           }
                       });
-                      console.log("Entering Lottery...");
-                      const tx = await lottery
-                          .connect(accounts[1])
-                          .enterLottery({ value: entranceFee });
+                      console.log("Entering Lottery--------------------");
+                      const tx = await lottery.enterLottery({ value: entranceFee });
                       await tx.wait(1);
-                      const winnerStartingBalance = await accounts[1].getBalance();
+                      const winnerStartingBalance = await accounts[0].getBalance();
                   });
               });
           });

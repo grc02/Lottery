d335bef9d334fdbd63312839ca63563f7fda0496
grc02 <georgi.chonkov27@gmail.com>
2022-11-22 22:14:07 +0200

    Added a script for entering the lottery

---
+++ scripts/enterLottery.js
@@ -0,0 +1,16 @@
+const { ethers } = require("hardhat");
+
+async function enterLottery() {
+    const lottery = await ethers.getContract("Lottery");
+    const entranceFee = await lottery.getEntranceFee();
+    const tx = await lottery.enterLottery({ value: entranceFee });
+    await tx.wait(1);
+    console.log("Raffle Entered!");
+}
+
+enterLottery()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
